2cc304978a569361bd1aa4690ffebd77306e7587
rayphelan <rayphelan@gmail.com>
2018-11-29 14:05:57 +0000

    Deploy Basic API Server

--- test/test.js
+++ test/test.js
@@ -2,23 +2,10 @@
 const expect = require('chai').expect;
 const axios = require('axios');
 
-// Testing if the API is running
-describe('testing that the API is running', function() {
-  it('should be running', function() {
-    axios.get('http://localhost:3000')
-      .then(response => {
-        expect(response.status).to.be.equal(200);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  });
-});
-
-// Testing the Companies endpoint
-describe('testing User endpoint', function() {
+// Testing the Base Get endpoint
+describe('testing Base endpoint', function() {
   it('should responde with Status 200', function() {
-    axios.get('http://localhost:3000/api/companies')
+    axios.get('http://localhost:3000/api/bases')
       .then(response => {
         expect(response.status).to.be.equal(200);
       });
