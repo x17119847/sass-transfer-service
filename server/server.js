85dcf06f039a43e37df89faef3e76da211545207
rayphelan <rayphelan@gmail.com>
2018-12-02 17:20:36 +0000

    Locked down API

--- server/server.js
+++ server/server.js
@@ -1,25 +1,26 @@
 'use strict';
 
-var loopback = require('loopback');
-var boot = require('loopback-boot');
+const loopback = require('loopback');
+const boot = require('loopback-boot');
+const app = module.exports = loopback();
 
-var app = module.exports = loopback();
+// Port
+const port = process.env.PORT || 3001;
 
+// Start the web server
 app.start = function() {
-  // start the web server
-  return app.listen(function() {
+  return app.listen(port, function() {
     app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
+    const baseUrl = app.get('url').replace(/\/$/, '');
     console.log('Web server listening at: %s', baseUrl);
     if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
+      const explorerPath = app.get('loopback-component-explorer').mountPath;
       console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
     }
   });
 };
 
 // Bootstrap the application, configure models, datasources and middleware.
-// Sub-apps like REST API are mounted via boot scripts.
 boot(app, __dirname, function(err) {
   if (err) throw err;
 
